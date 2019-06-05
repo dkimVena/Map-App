473232a31c6483cf26c65e0e5cf3ead0e6186fd0
goginim <david.ingook@gmail.com>
2019-06-05 06:44:52 -0400

    dev country information card

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Segment } from 'semantic-ui-react';
 // import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 import Dropdown from './Dropdown';
+import InfoCard from './InfoCard';
 
 import query from '../queries/fetchContinents';
 
@@ -17,6 +18,25 @@ class App extends React.Component {
         <Dropdown
           title="test"
           options={[{ name: 'test', key: 1 }, { name: 'test2', key: 2 }]}
+        />
+        <InfoCard
+          country={{
+            code: 'KR',
+            name: 'South Korea',
+            native: '대한민국',
+            phone: '82',
+            continent: {
+              name: 'Asia'
+            },
+            currency: 'KRW',
+            languages: [
+              {
+                name: 'Korean'
+              }
+            ],
+            emoji: '🇰🇷',
+            emojiU: 'U+1F1F0 U+1F1F7'
+          }}
         />
         <Segment>{continents.map(item => item.name)}</Segment>
       </React.Fragment>
---
+++ src/components/InfoCard/InfoCard.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import classNames from 'classnames';
+
+import './InfoCard.scss';
+
+class InfoCard extends Component {
+  getCountryLanguages = countries => countries.map(country => country.name);
+
+  render() {
+    const { className, country } = this.props;
+
+    return (
+      <figure className={classNames('InfoCard', className)}>
+        <div className="flag-image">
+          <span>{country.emoji}</span>
+        </div>
+        <figcaption>
+          <h3>{country.name}</h3>
+          <h4>{country.native}</h4>
+          <p>{country.code}</p>
+          <p>
+            <i className="icon ion-ios-telephone" />: +{country.phone}
+          </p>
+          <p>
+            <i className="icon ion-social-usd" />: {country.currency}
+          </p>
+          <p>
+            <i className="icon ion-chatbox-working" />:{' '}
+            {this.getCountryLanguages(country.languages).join(', ')}
+          </p>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
+export default InfoCard;
