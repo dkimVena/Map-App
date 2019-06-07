e6dddcab2f2c0cc3fdb3b42e7e6ea1cbe59e217c
goginim <david.ingook@gmail.com>
2019-06-07 10:57:53 -0400

    restructuring

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Layout.scss';
+
 const Layout = ({ children }) => {
   return <React.Fragment>{children}</React.Fragment>;
 };
--- src/components/MainPanel/MainPanel.js
+++ src/components/MainPanel/MainPanel.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import classNames from 'classnames';
 import Map from '../Map';
@@ -6,32 +6,27 @@ import locationData from '../../static/location.json';
 
 import './MainPanel.scss';
 
-class MainPanel extends Component {
-  getCountryLocation = country => {
-    let result = {};
-    if (Object.keys(country).length !== 0) {
-      let selectedCountry = locationData.ref_country_codes.find(
-        data => data.alpha2 === country.code
-      );
-      result = {
-        code: country.code,
-        name: country.name,
-        coordinates: [selectedCountry.longitude, selectedCountry.latitude]
-      };
-    }
-    return result;
-  };
-
-  render() {
-    return (
-      <div className={classNames('mainPanel')}>
-        <Map
-          country={this.getCountryLocation(this.props.selectedCountry)}
-          onSelectCountry={this.props.onSelectCountry}
-        />
-      </div>
+const getCountryLocation = country => {
+  let result = {};
+  if (Object.keys(country).length !== 0) {
+    let selectedCountry = locationData.ref_country_codes.find(
+      data => data.alpha2 === country.code
     );
+    result = {
+      code: country.code,
+      name: country.name,
+      coordinates: [selectedCountry.longitude, selectedCountry.latitude]
+    };
   }
-}
+  return result;
+};
+const MainPanel = ({ onSelectCountry, selectedCountry }) => (
+  <div className={classNames('mainPanel')}>
+    <Map
+      country={getCountryLocation(selectedCountry)}
+      onSelectCountry={onSelectCountry}
+    />
+  </div>
+);
 
 export default MainPanel;
